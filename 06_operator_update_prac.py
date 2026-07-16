860a0c18b3f791df28ca1af0f32eaf0b9c8baebb
vompos0179-prog <vompos0179@gmail.com>
2026-07-16 10:33:25 +0900

    연산자 실습 4개 추가

--- 06_operator_update_prac.py
+++ 06_operator_update_prac.py
@@ -25,3 +25,54 @@ print(9**2) # 81
 
 가로, 세로, 높이 = 3,4,5
 print(가로*세로*높이) # 60
+
+# "=========================================================
+print("=============실습 3============")
+
+print("hi" == "hello") # False
+print(5 != 6) # True
+print(5 > 6) # False
+print(5 < 6) # True
+print(5 >= 5) # True
+print(5 <= 5) # True
+
+# "=========================================================
+print("=============실습 4============")
+
+temp = 85
+temp_A = 60 < temp and temp < 90
+print(temp_A) # True
+pressure = 5
+pressure_A = pressure >= 3 and pressure <= 7
+print(pressure_A) # True
+print(temp_A and pressure_A) # True
+
+# "=========================================================
+print("=============실습 5============")
+
+stock = 100
+stock += 50
+print(stock) # 150
+stock -= 30
+print(stock) # 120
+stock += 5
+print(stock) # 125
+
+# "=========================================================
+print("=============실습 6============")
+
+total = 500
+fail = 23
+print(fail / total * 100) # 4.6
+
+run_A = 21
+total_run = 24
+print(run_A / total_run * 100) # 87.5
+
+# "=========================================================
+print("=============실습 7============")
+
+total_run = 500
+print(total_run // 60) # 8
+print(total_run % 60) # 20
+
